8f04b3f614cd1621a215180bd6af66ff2fec4793
fatma <fatmaalnaseebx2@gmail.com>
2025-02-11 21:20:42 +0300

    Added if-else if-else statement to handle zero case

---
+++ if_else_if.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val num = -23
+
+    if (num > 0) {
+        println("The number is positive")
+    }
+    else if (num < 0){
+        println("The number is negative")
+    }
+    else {
+        println("The number is zero")
+    }
+}
