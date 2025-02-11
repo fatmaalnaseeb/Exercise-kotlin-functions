6013abd62b2c1df7c3226a3b68867bac8838e7d6
fatma <fatmaalnaseebx2@gmail.com>
2025-02-11 23:01:08 +0300

    Added grade evaluation program

---
+++ grade_evaluation.kt
@@ -0,0 +1,22 @@
+fun main(){
+    val score  = 97
+    if (score > 90){
+        println("Grade: A")
+    }
+    else if ( score >= 89){
+        println("Grade: B")
+    }
+    else if (score >= 70){
+        println("Grade: c")
+    }
+    else if (score >= 60){
+        println("Grade: D")
+    }
+    else {
+        println("Grade: F")
+    }
+
+
+
+
+}
